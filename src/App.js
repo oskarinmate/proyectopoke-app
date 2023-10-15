662d9fa83aba732762b008609c62bcb5f639706b
gael <oskarinmate@gmail.com>
2023-10-14 20:38:55 -0600

    pokeapi v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+const axios = require('axios');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Nombre del Pokémon que deseas obtener
+const nombrePokemon = 'pikachu';
 
-export default App;
+// URL de la API de PokeAPI para obtener información de un Pokémon
+const apiUrl = `https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`;
+
+// Realiza una solicitud a la API
+axios.get(apiUrl)
+  .then(response => {
+    if (response.status === 200) {
+      const pokemon = response.data;
+      console.log('Nombre del Pokémon:', pokemon.name);
+      console.log('ID del Pokémon:', pokemon.id);
+      console.log('Tipos del Pokémon:');
+      pokemon.types.forEach(tipo => {
+        console.log('-', tipo.type.name);
+      });
+    } else {
+      console.log('No se encontró el Pokémon.');
+    }
+  })
+  .catch(error => {
+    console.error('Error al obtener datos del Pokémon:', error);
+  });
